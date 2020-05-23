4734e66cd662a063ee7c75122c657c4809fbe226
Andrew03 <andrewhoang03@gmail.com>
2020-05-23 03:05:43 -0700

    Updated import order

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,9 +3,10 @@ from flask_cors import CORS
 from flask_restful import Api
 from flask_restful_swagger import swagger
 from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+from app.endpoints import initialize_routes
 from config import Config
 
-db = SQLAlchemy()
 
 def create_app(for_testing=False):
     app = Flask(__name__)
@@ -18,9 +19,7 @@ def create_app(for_testing=False):
     app.app_context().push()
     CORS(app)
     api = swagger.docs(Api(app), apiVersion="0.1")
-    db.init_app(app)
 
-    # Weird import to avoid circular dependencies
-    from app.endpoints import initialize_routes
+    db.init_app(app)
     initialize_routes(api)
     return app
