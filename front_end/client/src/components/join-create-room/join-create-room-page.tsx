5617ca2124a88f25a9d7fc73ffc2cb0964546f97
Andrew03 <andrewhoang03@gmail.com>
2020-06-13 20:17:06 -0700

    Added video room name to video room page

--- front_end/client/src/components/join-create-room/join-create-room-page.tsx
+++ front_end/client/src/components/join-create-room/join-create-room-page.tsx
@@ -58,7 +58,10 @@ const JoinCreateRoomPage = (props: Prop) => {
 
     return (
         <div>
-            {NavBar("Logout", logoutClick)}
+            <NavBar
+                buttonName="Logout"
+                buttonOnClick={logoutClick}
+            />
             <RoomListR setPageForward={props.setPageForward}/>
         </div>
     )
