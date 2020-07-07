4365ece573a9c70343e8eb769759ff1d8e5ae7f4
Eric Pan <ericpan0207@gmail.com>
2020-07-06 17:26:32 -0700

    added redux devtools

--- front_end/client/src/store/index.tsx
+++ front_end/client/src/store/index.tsx
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { reducer } from "./reducer";
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
