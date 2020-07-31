bb2d1e4a128336b94920cedaf300f5536f8a88b5
Sung Ahn <sahn1998@gmail.com>
2020-07-30 17:53:23 -0700

    removed styles

--- front_end/client/src/components/video-room/user-list/user-list.tsx
+++ front_end/client/src/components/video-room/user-list/user-list.tsx
@@ -26,7 +26,7 @@ export interface Prop {
  */
 export const UserList = (props: Prop) => {
     return (
-        <div id="user-list" >
+        <div id="user-list">
             <Typography className="header">
                 ACTIVE USERS
             </Typography>
